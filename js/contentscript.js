3f211638fc148432a0b396cdfa7029b0502ebaa7
jasonhuang <1248394650@qq.com>
2021-12-17 10:00:30 +0800

    fix: 插件脚本延迟加载

--- js/contentscript.js
+++ js/contentscript.js
@@ -52,8 +52,8 @@ function getDownloadUrl() {
   return _link
 }
 
-function showModal() {
-  var message = `<div role="alert" class="el-message el-message--success" style="top: 230px; z-index: 2023;"><i class="el-message__icon el-icon-success"></i><p class="el-message__content">成功复制链接，可以直接下载或使用工具下载</p><!----></div>`
+function showModal(text) {
+  var message = `<div role="alert" class="el-message el-message--success" style="top: 230px; z-index: 2023;"><i class="el-message__icon el-icon-success"></i><p class="el-message__content">${text}</p><!----></div>`
   $(message).appendTo($('body'))
   setTimeout(() => {``
     $('.el-message').remove()
@@ -71,123 +71,114 @@ function copy(link) {
   }
   transfer.blur();
   document.body.removeChild(transfer);
-  showModal()
+  showModal('成功复制链接，可以直接下载或使用工具下载')
 }
 
 document.addEventListener('DOMContentLoaded', function () {
   injectLayui()
   injectCustomJs(null, function () {
+    var btns = `<div class="baidu_player_speed style="margin-top: 2px;">
+      <div class="layui-form-item" style="width: 800px;">
+        <div class="layui-inline">
+          <div class="layui-btn-group" id="bnts-group">
+            <button id="speed_1.0" data-value="1.0" type="button" class="layui-btn layui-btn-warm">正常速度</button>
+            <button id="speed_1.5" data-value="1.5" type="button" class="layui-btn">1.5倍速</button>
+            <button id="speed_2.0" data-value="2.0" type="button" class="layui-btn">2.0倍速</button>
+          </div>
+        </div>
+        <div class="layui-inline">
+          <label class="layui-form-label" style="width: 100px">自定义倍速</label>
+          <div class="layui-input-block">
+            <div class="layui-unselect layui-form-select">
+              <div class="layui-select-title">
+                <input type="text" placeholder="请选择" value="" readonly="" class="layui-input layui-unselect">
+                <i class="layui-edge"></i>
+              </div>
+              <dl class="layui-anim layui-anim-upbit" style="">
+                <dd lay-value="" class="layui-select-tips layui-this">请选择</dd>
+                <dd lay-value="1.5" class="">1.5</dd>
+                <dd lay-value="1.6" class="">1.6</dd>
+                <dd lay-value="1.7" class="">1.7</dd>
+                <dd lay-value="1.8" class="">1.8</dd>
+                <dd lay-value="1.9" class="">1.9</dd>
+                <dd lay-value="2.0" class="">2.0</dd>
+                <dd lay-value="2.5" class="">2.5</dd>
+                <dd lay-value="2.6" class="">2.6</dd>
+                <dd lay-value="2.7" class="">2.7</dd>
+                <dd lay-value="2.8" class="">2.8</dd>
+                <dd lay-value="2.9" class="">2.9</dd>
+                <dd lay-value="3.0" class="">3.0</dd>
+                <dd lay-value="10.0" class="">10.0</dd>
+                <dd lay-value="12.0" class="">12.0</dd>
+                <dd lay-value="16.0" class="">16.0</dd>
+              </dl>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>`
+    console.log($('#app'))
+    window.$app = $('#app')
+    $('#app').append(btns)
+    // video里插入按钮---shadow dom mode为close无法控制
+    // var fullscreenSpeed = $(`<div class="speed-json-button-group">倍速</div>`)
+    // fullscreenSpeed.insertAfter('.video-functions-tips .tips-ul .video-functions-last')
+    Object.defineProperty(this, 'navigator', { value: { platform: '' } })
+  
+    // 获取大文件直链
+    // var _link = getDownloadUrl()
+    $('#bnts-group button').on('click', function (e) {
+      var current = e.currentTarget
+      var speed = parseInt(current.dataset.value)
+      $(this).addClass('layui-btn-warm').siblings().removeClass('layui-btn-warm')
+      // 下拉列表保持和按钮组一致
+      $('.layui-select-tips').addClass('layui-this').siblings().removeClass('layui-this')
+      $('.layui-input').val('')
+      window.postMessage({ type: 'to_inject', speed: speed }, '*')
+    })
+    $('.layui-form-select').on('click', function (e) {
+      var classNames = $(this).prop('className')
+      if (classNames.includes('layui-form-selected')) {
+        $(this).removeClass('layui-form-selected')
+      } else {
+        $(this).addClass('layui-form-selected')
+      }
+    })
+    $('.layui-anim dd').on('click', function (e) {
+      $(this).addClass('layui-this').siblings().removeClass('layui-this')
+      var speed = $(this).text()
+      $('.layui-input').val(speed)
+      if ($(`button[data-value="${speed}"]`).length > 0) {
+        $(`button[data-value="${speed}"]`).addClass('layui-btn-warm').siblings().removeClass('layui-btn-warm')
+      } else {
+        $('.layui-btn').removeClass('layui-btn-warm')
+      }
+      if (!isNaN(parseFloat(speed))) {
+        window.postMessage({ type: 'to_inject', speed: speed }, '*')
+      }
+    })
+    $('.layui-form-switch').on('click', function (e) {
+      var classNames = $(this).prop('className')
+      if (classNames.includes('layui-form-onswitch')) {
+        $(this).removeClass('layui-form-onswitch')
+        $('.layui-form-switch em').text('OFF')
+        $('.suspended-ball').css('display', 'none')
+      } else {
+        $(this).addClass('layui-form-onswitch')
+        $('.layui-form-switch em').text('ON')
+        $('.suspended-ball').css('display', 'inline-block')
+      }
+    })
+    // $('#copy_download_link').on('click', function (e) {
+    //   e.preventDefault();
+    //   copy(_link)
+    // })
+    $(document).on('click', function(e) {
+      if($(e.target).closest('.layui-form-select').length === 0) {
+        $('.layui-form-select').removeClass('layui-form-selected')
+      }
+    })
 
   }, false)
 
-  // console.log(window.videojs) undefined
-  var videoContent = document.getElementsByClassName('video-content')[0]
-  var videoTitle = document.getElementsByClassName('video-title')[0]
-  var videoWrapOuter = videoTitle.nextElementSibling
-  var btns = parseDom(`<div class="baidu_player_speed ">
-    <div class="layui-form-item">
-      <div class="layui-inline">
-        <div class="layui-btn-group" id="bnts-group">
-          <button id="speed_1.0" data-value="1.0" type="button" class="layui-btn layui-btn-warm">正常速度</button>
-          <button id="speed_1.5" data-value="1.5" type="button" class="layui-btn">1.5倍速</button>
-          <button id="speed_2.0" data-value="2.0" type="button" class="layui-btn">2.0倍速</button>
-        </div>
-      </div>
-      <div class="layui-inline">
-        <label class="layui-form-label">自定义倍速</label>
-        <div class="layui-input-block">
-          <div class="layui-unselect layui-form-select">
-            <div class="layui-select-title">
-              <input type="text" placeholder="请选择" value="" readonly="" class="layui-input layui-unselect">
-              <i class="layui-edge"></i>
-            </div>
-            <dl class="layui-anim layui-anim-upbit" style="">
-              <dd lay-value="" class="layui-select-tips layui-this">请选择</dd>
-              <dd lay-value="1.5" class="">1.5</dd><dd lay-value="1.6" class="">1.6</dd>
-              <dd lay-value="1.7" class="">1.7</dd><dd lay-value="1.8" class="">1.8</dd>
-              <dd lay-value="1.9" class="">1.9</dd><dd lay-value="2.0" class="">2.0</dd>
-              <dd lay-value="2.5" class="">2.5</dd><dd lay-value="2.6" class="">2.6</dd>
-              <dd lay-value="2.7" class="">2.7</dd><dd lay-value="2.8" class="">2.8</dd>
-              <dd lay-value="2.9" class="">2.9</dd><dd lay-value="3.0" class="">3.0</dd>
-            </dl>
-          </div>
-        </div>
-      </div>
-      <div class="layui-inline">
-        <label class="layui-form-label">开启悬浮窗</label>
-        <div class="layui-input-block">
-          <input type="checkbox" name="switch" lay-skin="switch" lay-text="ON|OFF" lay-filter="switchTest" value="1" style="display: none;">
-          <div class="layui-unselect layui-form-switch" lay-skin="_switch">
-            <em>OFF</em>
-            <i></i>
-          </div>
-        </div>
-      </div>
-      <div class="layui-inline">
-        <button id="copy_download_link" type="button" class="btn-primary">提取下载直链</button>
-      </div>
-    </div>
-  </div>`)[0]
-  videoContent.insertBefore(btns, videoWrapOuter)
-  // video里插入按钮---shadow dom mode为close无法控制
-  // var fullscreenSpeed = $(`<div class="speed-json-button-group">倍速</div>`)
-  // fullscreenSpeed.insertAfter('.video-functions-tips .tips-ul .video-functions-last')
-  Object.defineProperty(this, 'navigator', { value: { platform: '' } })
-  // video无法覆盖
-  $('#video-wrap-outer').append(`<div class="suspended-ball movable" id="moveDiv">
-    <div id="inner"></div>
-  </div>`)
-  // 获取大文件直链
-  var _link = getDownloadUrl()
-  $('#bnts-group button').on('click', function (e) {
-    var current = e.currentTarget
-    var speed = parseInt(current.dataset.value)
-    $(this).addClass('layui-btn-warm').siblings().removeClass('layui-btn-warm')
-    // 下拉列表保持和按钮组一致
-    $('.layui-select-tips').addClass('layui-this').siblings().removeClass('layui-this')
-    $('.layui-input').val('')
-    window.postMessage({ type: 'to_inject', speed: speed }, '*')
-  })
-  $('.layui-form-select').on('click', function (e) {
-    var classNames = $(this).prop('className')
-    if (classNames.includes('layui-form-selected')) {
-      $(this).removeClass('layui-form-selected')
-    } else {
-      $(this).addClass('layui-form-selected')
-    }
-  })
-  $('.layui-anim dd').on('click', function (e) {
-    $(this).addClass('layui-this').siblings().removeClass('layui-this')
-    var speed = $(this).text()
-    $('.layui-input').val(speed)
-    if ($(`button[data-value="${speed}"]`).length > 0) {
-      $(`button[data-value="${speed}"]`).addClass('layui-btn-warm').siblings().removeClass('layui-btn-warm')
-    } else {
-      $('.layui-btn').removeClass('layui-btn-warm')
-    }
-    if (!isNaN(parseFloat(speed))) {
-      window.postMessage({ type: 'to_inject', speed: speed }, '*')
-    }
-  })
-  $('.layui-form-switch').on('click', function (e) {
-    var classNames = $(this).prop('className')
-    if (classNames.includes('layui-form-onswitch')) {
-      $(this).removeClass('layui-form-onswitch')
-      $('.layui-form-switch em').text('OFF')
-      $('.suspended-ball').css('display', 'none')
-    } else {
-      $(this).addClass('layui-form-onswitch')
-      $('.layui-form-switch em').text('ON')
-      $('.suspended-ball').css('display', 'inline-block')
-    }
-  })
-  $('#copy_download_link').on('click', function (e) {
-    e.preventDefault();
-    copy(_link)
-  })
-  $(document).on('click', function(e) {
-    if($(e.target).closest('.layui-form-select').length === 0) {
-      $('.layui-form-select').removeClass('layui-form-selected')
-    }
-  })
 })
